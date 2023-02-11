d36f9d3a19776810cb6b6a6919a3c03493c415d6
hujuu <1961946+hujuu@users.noreply.github.com>
2023-02-11 09:22:25 +0900

    Auth0 base setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+"use client";
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import React from 'react';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -72,19 +80,18 @@ export default function Home() {
           <p className={inter.className}>Explore the Next.js 13 playground.</p>
         </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <a href="/api/auth/login">Login</a>
+
+        <a href="/api/auth/logout">Logout</a>
+
+        {user && (
+            <div>
+              <img src={user.picture} alt={user.name} />
+              <h2>{user.name}</h2>
+              <p>{user.email}</p>
+            </div>
+        )}
+
       </div>
     </main>
   )
